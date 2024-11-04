f7e2dccbd618e5bcc5ac6a9cb07472104a103dea
Yara1209 <160124985+Yara1209@users.noreply.github.com>
2024-11-04 11:44:44 -0300

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,9 @@
-function nomeDafuncao{
+function criaCartao(tema,pergunta,resposta){
+const container=document.getElementByID(container);
+let cartao=document.creatElement()
+cartao.className=cartao;
+cartao.innerHTML=
 
-}
-function criaCartao(3TRIMESTRE-2F-main/assets/main.jstema,pergunta,resposta)
+
+criaCartao('HISTÓRIA','em que ano iniciou-se a guerra do vietnã?,'em 1955');
+criaCartao
