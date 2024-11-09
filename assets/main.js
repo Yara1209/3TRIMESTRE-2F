8e2895e1c7e2fd433fc81762fcc7f624c4c671f7
Yara1209 <160124985+Yara1209@users.noreply.github.com>
2024-11-09 09:43:37 -0300

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,20 @@
 function criaCartao(tema,pergunta,resposta){
-const container=document.getElementByID(container);
-let cartao=document.creatElement()
-cartao.className=cartao;
-cartao.innerHTML=
-
-
-criaCartao('HISTÓRIA','em que ano iniciou-se a guerra do vietnã?,'em 1955');
-criaCartao
+    const container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class="cartao-conteudo">
+     <h3> ${tema}</h3>
+    <div class="cartao-conteudo-pergunta">
+        ${pergunta}
+    </div>
+    <div class="cartao-conteudo-resposta">
+    ${resposta}
+    </div>
+    </div>
+    `
+    container.appendChild(cartao)
+    
+}
+criaCartao('Historia','Em que ano iniciou  a guerra do Vietnã?','Em 1955')
+criaCartao('Portugues', 'O que é paraxitona?','Uma paroxitona e uma palavra cuja silaba tonica é a penultima.')
